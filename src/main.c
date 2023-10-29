5ea80f37493d8882402be673b9f002e55ae2d04c
Daksh <dakshkaul7790@gmail.com>
2023-10-29 13:21:32 +0530

    removed build file and removed main.c gtk

--- src/main.c
+++ src/main.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <gtk/gtk.h>
+#include <string.h>
 
-int main(int argc, char* argv[]){
+int main(int argc, char** argv){
 
-  return 0;
+  return 0;  
 }
