17655600dc9486c6e05de914670109ab0603a507
Daksh <dakshkaul7790@gmail.com>
2023-10-30 23:58:43 +0530

    Created Server & Client Code. Compiled Code. Updated README To Correct Project Details & Lesson Notes. Also Added Citations

--- README.md
+++ README.md
@@ -1,3 +1,53 @@
-# Recipe Suggesting App
+# Simple C Client-Server Application
 
+Learing how to create a socket-based client-sever system in C.
 
+## Client Structure
+
+1. Socket for client is created by ```socket()```
+
+2. Client socket is created, and settings for socket are defined
+
+   - Domain: IPv4 (AF_INET)
+   - Type Of Socket: TCP/UDP (SOCK_STREAM)
+   - Protocol: IP (0)
+
+3. Attempt to connect to remote address/port number with ```connect()```
+
+4. Request Data Receival From Server via ```recv()```
+
+5. Connection To Remote Server Is Establised
+
+6. Data Is received from server via ```recv()``` -> a string
+
+## Server Structure
+
+1. Socket for client is created by ```socket()```
+
+2. Client socket is created, and settings for socket are defined
+
+   - Domain: IPv4 (AF_INET)
+   - Type Of Socket: TCP/UDP (SOCK_STREAM)
+   - Protocol: IP (0)
+
+3. Create Data - In This Case A Message - To Send To Client Node
+
+4. Bind To A Empty Port In Network via ```bind()```
+
+5. Set Server To Listen For Connection Attempts via ```listen()```
+
+6. Send Data From Server To Client via ```send()```
+
+## Useage
+
+1. Open 2 Terminals
+
+2. Run ```server``` on one
+
+3. Run ```client``` on other
+
+4. Data Should Be Send To Other Terminal & Displayed
+
+### Acknowledgements
+
+- [geeksforgeeks][https://www.geeksforgeeks.org/simple-client-server-application-in-c/]
--- src/client.c
+++ src/client.c
@@ -5,41 +5,54 @@
 #include <errno.h>
 #include <string.h>
 #include <stdlib.h>
+#include <unistd.h>
 
-int main(int argc, char** argv){
+int main(int argc, char** argv) {
+    // Creating a client socket via socket() function call
+    int clientSocket = socket(AF_INET, SOCK_STREAM, 0);
 
-  // Creating A Server Socket via socket() function call
-  int socket = socket(AF_INET, SOCK_STREAM, 0);
-
-  // Creating Struct That Holds Server Socket Template
-  struct sockaddr_in server_addr;
-
-  // Setting Server Settings/Configs
-  server_addr.sin_family = AF_INET; 
-  server_addr.sin_port   = htons(999); /* Using random unused port */
-  server_addr.sin_addr.s_addr = INADDR_ANY;
-
-  // Establishing Connect With Socket, And Storing Attempt Result
-  int connectStatus = connect(socket, (struct sockaddr*)&server_addr, sizeof(server_addr));
-
-  if (connectStatus == -1){
-    perror("Socket Connect Attempt Failed, Exiting With [-1] Status.\n");
-  } else {
-
-    // Creating A Buffer For Data
-    char buffer[256];
-
-    // Reads Data From Socket -> Buffer
-    recv(socket, buffer, sizeof(buffer), 0);
-
-    // Checking If Socket Data Is Correctly Loaded Into Buffer
-    if (atoi(buffer) == -1){
-      perror("Could Not Read Data Into Buffer From Buffer. Exiting With [-1] Status.\n");
+    // Check if socket creation failed
+    if (clientSocket == -1) {
+        perror("Socket creation failed");
+        exit(EXIT_FAILURE);
     }
 
-    // Outputting Data From Socket
-    printf("Message From Socket: %s\n", buffer);
-  }
-  
-  return 0;  
+    // Creating struct that holds server socket template
+    struct sockaddr_in server_addr;
+
+    // Setting server settings/configs
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(9001); // Using the same port as the server
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+
+    // Establishing connection with the server socket
+    int connectStatus = connect(clientSocket, (struct sockaddr*)&server_addr, sizeof(server_addr));
+
+    if (connectStatus == -1) {
+        perror("Socket connect attempt failed, exiting with [-1] status.\n");
+        close(clientSocket); // Close the client socket before exiting
+        exit(EXIT_FAILURE);
+    } else {
+        // Creating a buffer for data
+        char buffer[256];
+
+        // Reading data from socket -> buffer
+        ssize_t bytesReceived = recv(clientSocket, buffer, sizeof(buffer), 0);
+
+        // Checking if socket data is correctly loaded into the buffer
+        if (bytesReceived == -1) {
+            perror("Could not read data into buffer from socket. Exiting with [-1] status.\n");
+        } else {
+            // Null-terminate the received data to make it a valid C string
+            buffer[bytesReceived] = '\0';
+
+            // Outputting data from the socket
+            printf("Message from server: %s\n", buffer);
+        }
+    }
+
+    // Close the client socket
+    close(clientSocket);
+
+    return 0;
 }
